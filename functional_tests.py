f480f3c324697fdba42d3359f15d797edbede1c6
febyola tiara putri <febyola.tiara@ui.ac.id>
2015-09-19 17:25:39 +0000

    commet teardown

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -7,13 +8,14 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+	#def tearDown(self):
+		#self.browser.quit()
 
 	def test_can_start_a_list_and_retrive_it_later(self):
 		self.browser.get('http://localhost:8000')
 		
 		self.assertIn('To-Do',self.browser.title)
+		header_text = self.browser.find_element
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
